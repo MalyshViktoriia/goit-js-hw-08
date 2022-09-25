507df841e9a6a7f4cda7485934afbce238d3eb5e
Viktoriia Malysh <pylypenko.viktoriia@gmail.com>
2022-09-25 11:05:07 +0200

    add the second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+
+
+const onPlay = function ({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentLocalData = localStorage.getItem('videoplayer-current-time');
+
+if (currentLocalData) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+};
